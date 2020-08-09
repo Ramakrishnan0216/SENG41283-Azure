c4ab16e1b78fd927c00c410274aaf20d10604c53
R.Ramakrishnan <>
2020-08-10 01:16:46 +0530

    Cors configuration.

--- backend/main-service/src/main/java/se/web/ramakrishnan/mainservice/controller/EmployeeController.java
+++ backend/main-service/src/main/java/se/web/ramakrishnan/mainservice/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"*"})
 public class EmployeeController {
 
     @Autowired
