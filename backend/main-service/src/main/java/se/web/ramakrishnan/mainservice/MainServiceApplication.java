ca066f4d1bb9f5ce24dcb9574b6c9cb0ddc0f6ee
R.Ramakrishnan <>
2020-08-09 22:58:19 +0530

    Cors changes.

--- backend/main-service/src/main/java/se/web/ramakrishnan/mainservice/MainServiceApplication.java
+++ backend/main-service/src/main/java/se/web/ramakrishnan/mainservice/MainServiceApplication.java
@@ -3,9 +3,10 @@ package se.web.ramakrishnan.mainservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
-public class MainServiceApplication {
+public class MainServiceApplication  extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainServiceApplication.class, args);
