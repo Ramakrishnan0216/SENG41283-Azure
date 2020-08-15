05167319aa463cbf1c020a018635525197bbecb5
R.Ramakrishnan <>
2020-08-16 03:28:58 +0530

    deploying employee service.

--- frontend/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  apiBaseUrl: 'http://localhost:8080/api/employees',
-  appClientID: 'clientId',
-  appTenantID:'TenantId',
-  loginRedirectUriAD:'http://localhost:4200/employees',
-  logoutRedirectUriAD:'http://localhost:4200/',
-};
